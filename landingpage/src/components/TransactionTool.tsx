938f968ab09421ccfe15edfdcf6a182113ce6b6a
Michi <michi.spebach@gmail.com>
2024-10-16 15:00:31 +0200

    extract function generateTransaction(...)

--- landingpage/src/components/TransactionTool.tsx
+++ landingpage/src/components/TransactionTool.tsx
@@ -10,27 +10,29 @@ interface TransactionToolProps {
 
 export const TransactionTool: React.FC<TransactionToolProps> = ({ name, mode }) => {
 	const [senderAddress, setSenderAddress] = useState('')
-	//const [senderAddressError, setSenderAddressError] = useState<Error|undefined>(undefined)
-	let senderAddressError: Error|undefined = undefined
 	const [senderUtxo, setSenderUtxo] = useState<{hash: string, index: number}|Error|'fetching'>('fetching')
 	const [inscriptions, setInscriptions] = useState([''])
-	const [transaction, setTransaction] = useState(generateClaimNameTransaction(name))
+	const [transaction, setTransaction] = useState<{
+		transaction: {toHex: () => string}
+		senderAddressError?: Error
+		senderUtxoError?: Error
+	} | undefined>(undefined)
 
 	useEffect(() => {
 		try {
-			setSenderUtxo({hash: senderAddress, index: 0}) // TODO: bitcoinExplorer.getOldestUtxoOfAddress(address).then(...)
+			setSenderUtxo({hash: util.asciiToHex(senderAddress).substring(0, 64), index: 0}) // TODO: bitcoinExplorer.getOldestUtxoOfAddress(address).then(...)
 		} catch (error: unknown) {
-			if (error instanceof Error) {
-				setSenderUtxo(error)
-			} else {
-				setSenderUtxo(new Error(String(error)))
-			}
+			setSenderUtxo(toError(error))
 		}
 	}, [senderAddress])
 
 	useEffect(() => {
-		setTransaction(generateClaimNameTransaction(name))
-	}, [name, senderUtxo, inscriptions])
+		if (senderUtxo !== 'fetching' && !(senderUtxo instanceof Error)) {
+			setTransaction(generateTransaction({name, senderAddress, senderUtxo, inscriptions, mode}))
+		}
+	}, [name, senderAddress, senderUtxo, inscriptions, mode])
+
+	const validTransaction: boolean = !!inscriptions[0] && !transaction?.senderAddressError && !transaction?.senderUtxoError
 
 	return (
 		<div>
@@ -58,60 +60,79 @@ export const TransactionTool: React.FC<TransactionToolProps> = ({ name, mode }) 
 					className="bg-white/15 border"
 				/>
 			</label>
-			<MarkedTextWithCopy clickToCopy>
-				{transaction.toHex()}
-			</MarkedTextWithCopy>
-			{senderAddressError && <div>{String(senderAddressError)}</div>}
-			{senderUtxo === 'fetching' && <div>Fetching senderUtxo...</div>}
-			{senderUtxo instanceof Error && <div>Error while getting senderUtxo from 'Your Address': {String(senderUtxo)}</div>}
+			<div style={validTransaction ? {} : {pointerEvents: 'none', userSelect: 'none', opacity: '0.5'}}>
+				<MarkedTextWithCopy clickToCopy>
+					{transaction?.transaction.toHex()}
+				</MarkedTextWithCopy>
+			</div>
+			{!senderAddress
+				? <div>Input 'Your Address' to generate a valid transaction.</div>
+				: <div>
+					{transaction?.senderAddressError && <div>{String(transaction.senderAddressError)}</div>}
+					{senderUtxo === 'fetching' && <div>Fetching UTXO...</div>}
+					{senderUtxo instanceof Error && <div>Error while fetching UTXO from 'Your Address': {String(senderUtxo)}</div>}
+					{transaction?.senderUtxoError && <div>Error with UTXO of 'Your Address': {String(transaction.senderUtxoError)}</div>}
+					{!inscriptions[0] && !transaction?.senderAddressError && !transaction?.senderUtxoError && <div>Input at least one 'Inscription'.</div>}
+				</div>
+			}
 		</div>
 	)
-	
-	function generateClaimNameTransaction(name: string): {toHex: () => string} {
-		//const transaction = new Transaction()
-		//transaction.addInput(util.hexToBytes('TODO'), 0) // TODO: input needs to be already added to be able to import transaction into wallet
-		//transaction.addOutput(util.hexToBytes(util.asciiToHex(plebAddress)), BigInt(546))
-		const transaction = new Psbt()
+}
 
-		if (!(senderUtxo instanceof Error) && senderUtxo !== 'fetching') {
-			try {
-				transaction.addInput(senderUtxo)
-			} catch (error: unknown) {
-				if (error instanceof Error) {
-					setSenderUtxo(error)
-				} else {
-					setSenderUtxo(new Error(String(error)))
-				}
-			}
-		}
+function generateTransaction(options: {
+	name: string
+	senderAddress: string
+	senderUtxo: {hash: string, index: number}
+	inscriptions: string[]
+	mode: 'claimAndInscribe'|'inscribe'
+}): {
+	transaction: {toHex: () => string}
+	senderAddressError?: Error
+	senderUtxoError?: Error
+} {
+	//const transaction = new Transaction()
+	//transaction.addInput(util.hexToBytes('TODO'), 0) // TODO: input needs to be already added to be able to import transaction into wallet
+	//transaction.addOutput(util.hexToBytes(util.asciiToHex(plebAddress)), BigInt(546))
+	const transaction = new Psbt()
+	let senderAddressError: Error|undefined = undefined
+	let senderUtxoError: Error|undefined = undefined
 
-		if (mode === 'claimAndInscribe') {
-			const plebAddress = util.generateBech32AddressWithPad(util.normalizeAsciiToBech32(name))
-			transaction.addOutput({
-				address: plebAddress,
-				value: BigInt(546)
-			})
-		}
-
-		for (const inscription of inscriptions) {
-			transaction.addOutput({script: new Uint8Array([script.OPS['OP_RETURN'], ...asciiToBytes(inscription)]), value: BigInt(0)})
-		}
-
-		try {
-			transaction.addOutput({address: senderAddress, value: BigInt(21_000_000*10**8)}) // TODO: transfer rest to sender
-			//setSenderAddressError(undefined)
-		} catch (error: unknown) {
-			if (error instanceof Error) {
-				senderAddressError = error
-				//setSenderAddressError(error)
-			} else {
-				senderAddressError = new Error(String(error))
-				//setSenderAddressError(new Error(String(error)))
-			}
-		}
-
-		return transaction
+	try {
+		transaction.addInput(options.senderUtxo)
+	} catch (error: unknown) {
+		senderUtxoError = toError(error)
 	}
+
+	if (options.mode === 'claimAndInscribe') {
+		const plebAddress = util.generateBech32AddressWithPad(util.normalizeAsciiToBech32(options.name))
+		transaction.addOutput({
+			address: plebAddress,
+			value: BigInt(546)
+		})
+	}
+
+	for (const inscription of options.inscriptions) {
+		transaction.addOutput({script: new Uint8Array([script.OPS['OP_RETURN'], ...asciiToBytes(inscription)]), value: BigInt(0)})
+	}
+
+	try {
+		transaction.addOutput({address: options.senderAddress, value: BigInt(21_000_000*10**8)}) // TODO: transfer rest to sender
+	} catch (error: unknown) {
+		senderAddressError = toError(error)
+	}
+
+	return {
+		transaction,
+		senderAddressError,
+		senderUtxoError
+	}
+}
+
+function toError(errorOfUnknownType: unknown): Error {
+	if (errorOfUnknownType instanceof Error) {
+		return errorOfUnknownType
+	}
+	return new Error(String(errorOfUnknownType))
 }
 
 /** @deprecated add this to util instead */
